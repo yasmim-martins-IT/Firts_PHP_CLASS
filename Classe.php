2c94b1ee1db3b48999d57a766188452bcb7523b3
yasmim-martins-IT <yasmim.martins.profissional@gmail.com>
2026-03-04 20:36:35 -0300

    feat : adicionando metodos

--- Classe.php
+++ Classe.php
@@ -7,6 +7,14 @@ class Cliente{
 
     public $nome ; //atributos
     public $cpf ;
+
+    public function mostrarDados() : void {
+        echo "mostrando meus dados" ;
+    }
+
+    public function mostrarCPF(string $cpf) : void {
+        echo "CPF: {$this->cpf} atualizado é {$cpf}" . PHP_EOL;
+    }
 }
 
 $c1 = new Cliente ; //instancias
@@ -17,8 +25,11 @@ $c1 -> nome = "Yasmim";
 $c1 -> cpf = "2322323232";
 $c2 -> cpf = "1292090192" ; 
 
+$c1 ->mostrarDados();
+$c1 ->mostrarCPF(4545545454)
 
-var_dump($c2 , $c1) ;  //get dos valores
+
+// var_dump($c2 , $c1) ;  //get dos valores
 
 
 ?>
